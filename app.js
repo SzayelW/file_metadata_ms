1425c510ea0b99d3c858b88e683b63bf8fa7e9e7
Andres <andres70p32@gmail.com>
2017-04-15 06:00:39 +0000

    basic upload form working

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ var multer = require('multer')
 var upload = multer({ dest: 'uploads/' })
 var app = express()
 
+app.get('/', (req, res)=>{
+    res.sendFile('./index.html', {root: __dirname })
+})
 
+app.post('/', upload.single('file'),(req, res)=>{
+
+    if(req.file){
+        var size = req.file.size;
+        res.json({size: size})        
+    }else{
+        res.json({error: "error uploading file"})
+    }
+})
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log("listening on port: " +  process.env.PORT || "8080")
